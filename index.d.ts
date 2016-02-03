84a6fbb27ed636e70715fe75327ca441bf56ad35
Max Battcher <me@worldmaker.net>
2016-02-03 15:16:33 -0500

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,13 +1,13 @@
-declare function createBlob(parts: any[], options?: { type: string }): Blob
-declare function createObjectURL(blob: Blob): string
-declare function revokeObjectURL(url: string): void
-declare function blobToBinaryString(blob: Blob): Promise<string>
-declare function binaryStringToBlob(binary: string, type?: string): Promise<Blob>
-declare function blobToBase64String(blob: Blob): Promise<string>
-declare function base64StringToBlob(base64: string, type?: string): Promise<Blob>
-declare function dataURLToBlob(dataURL: string): Promise<Blob>
-declare function imgSrcToDataURL(src: string, type?: string, crossOrigin?: string): Promise<string>
-declare function canvasToBlob(canvas: HTMLCanvasElement, type?: string): Promise<Blob>
-declare function imgSrcToBlob(src: string, type?: string, crossOrigin?: string): Promise<Blob>
-declare function arrayBufferToBlob(arrayBuff: ArrayBuffer, type?: string): Promise<Blob>
-declare function blobToArrayBuffer(blob: Blob): Promise<ArrayBuffer>er file contents here
+declare export function createBlob(parts: any[], options?: { type: string }): Blob
+declare export function createObjectURL(blob: Blob): string
+declare export function revokeObjectURL(url: string): void
+declare export function blobToBinaryString(blob: Blob): Promise<string>
+declare export function binaryStringToBlob(binary: string, type?: string): Promise<Blob>
+declare export function blobToBase64String(blob: Blob): Promise<string>
+declare export function base64StringToBlob(base64: string, type?: string): Promise<Blob>
+declare export function dataURLToBlob(dataURL: string): Promise<Blob>
+declare export function imgSrcToDataURL(src: string, type?: string, crossOrigin?: string): Promise<string>
+declare export function canvasToBlob(canvas: HTMLCanvasElement, type?: string): Promise<Blob>
+declare export function imgSrcToBlob(src: string, type?: string, crossOrigin?: string): Promise<Blob>
+declare export function arrayBufferToBlob(arrayBuff: ArrayBuffer, type?: string): Promise<Blob>
+declare export function blobToArrayBuffer(blob: Blob): Promise<ArrayBuffer>
